4cb2148e2207e54d09f1db865c1ab40a6ed590a2
INDRA\fhenriqued <fhenriqued@indracompany.com>
2020-11-18 12:01:38 -0300

    Ajuste para uso da api gateway

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -1,8 +1,11 @@
 package com.devsuperior.hrworker.resources;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +19,13 @@ import com.devsuperior.hrworker.repotitories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
 	@Autowired
 	private WorkerRepository  repository;
+	
+	@Autowired
+	private Environment env;
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
@@ -28,8 +36,17 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
-		Worker obj= repository.findById(id).get();
 		
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
+		Worker obj= repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
 
